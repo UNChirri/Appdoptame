4546d04e340e38b7a5912dc8a1a23d8993f301f4
afcajamarcar <afcajamarcar@unal.edu.co>
2017-09-27 09:35:57 -0500

    Splash screen added

---
+++ app/src/main/java/com/appdoptame/appdoptame/Base/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.appdoptame.appdoptame.Base;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.appdoptame.appdoptame.FBAuth.FBLogin;
+import com.appdoptame.appdoptame.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //Unless there's a heavy load, no handler/delayer is needed
+        
+        Intent intent = new Intent(SplashActivity.this, FBLogin.class);
+        startActivity(intent);
+        finish();
+    }
+}
