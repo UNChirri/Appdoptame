015dabf15eeddfbcdabd6259f2679fa2c3928db8
afcajamarcar <afcajamarcar@unal.edu.co>
2017-11-06 23:33:19 -0500

    Added profile image to user profile.

--- app/src/main/java/com/appdoptame/appdoptame/fragments/UserProfileFragment.java
+++ app/src/main/java/com/appdoptame/appdoptame/fragments/UserProfileFragment.java
@@ -1,12 +1,35 @@
 package com.appdoptame.appdoptame.fragments;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.appdoptame.appdoptame.Base.BaseActivity;
+import com.appdoptame.appdoptame.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
+
+import javax.net.ssl.HttpsURLConnection;
 
 /**
  * Created by crow on 6/11/17.
  */
 
 public class UserProfileFragment extends Fragment {
+
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
     public UserProfileFragment() {
         // Required empty public constructor
     }
@@ -15,5 +38,46 @@ public class UserProfileFragment extends Fragment {
     public static UserProfileFragment newInstance() {
         UserProfileFragment fragment = new UserProfileFragment();
         return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_userprofile, container, false);
+        ImageView profilepic = (ImageView) view.findViewById(R.id.profilepic);
+        try {
+            Bitmap returned_bitmap = new convertUrlToBitmap().execute().get();
+            profilepic.setImageBitmap(returned_bitmap);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        return view;
+        
+    }
+
+    private class convertUrlToBitmap extends AsyncTask<Void,Void,Bitmap> {
+        @Override
+        protected Bitmap doInBackground(Void... params) {
+            Bitmap myBitmap = null;
+            try {
+                URL url = new URL(user.getPhotoUrl().toString());
+                HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+                connection.setDoInput(true);
+                connection.connect();
+                InputStream input = connection.getInputStream();
+                myBitmap = BitmapFactory.decodeStream(input);
+
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+            return myBitmap;
+        }
     }
 }
